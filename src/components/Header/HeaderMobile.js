ad97771f2c511ccf4f7956899892d628034b779e
sina malmir <sinamalmir2@gmail.com>
2022-07-25 22:40:19 +0430

    mobile header design completed & start make a hero slider

--- src/components/Header/HeaderMobile.js
+++ src/components/Header/HeaderMobile.js
@@ -1,9 +1,78 @@
 import React from 'react';
 
+// images
+import Banner from '../../assets/img/topBanner.gif'
+import Logo from '../../assets/img/logo.svg';
+
 const HeaderMobile = () => {
     return (
         <div className='lg:hidden'>
-            header mobile
+            <div className='w-full  flex items-center '>
+                <img src={Banner} alt="top-banner" className=' w-max ' />
+            </div>
+            <div className='flex items-center justify-between p-3'>
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </span>
+                <img src={Logo} alt="logo" className='w-24' />
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </span>
+            </div>
+            <hr />
+
+            <div className='flex items-center justify-between p-2  '>
+               
+                <div className='relative w-4/5'>
+                    <label htmlFor="search" className='absolute top-3 z-10 right-2 text-[#A0A3A7]'>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
+                    </label>
+                    <input type="srarch" placeholder='جستجو' className='bg-[#F0F0F1] p-3 rounded-lg relative pr-10 text-xs w-full ' />
+                </div>
+
+                <div className='flex items-center justify-end mr-3'>
+                        <div className='flex items-center  text-sm rounded-lg  cursor-pointer ml-2 p-2'>
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                                </svg>
+                            </span>
+                            <span> ورود  </span>
+                        </div>
+                        <span className='cursor-pointer'>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                            </svg>
+                        </span>
+                </div>
+            </div>
+
+            <hr />
+
+            <div className='flex items-center justify-between p-2'>
+               <div className='flex items-center text-xs'>
+                <span className='ml-1 text-[#f9a825]'>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                </span>
+                <span>  لطفا شهر خود را انتخاب کنید </span>
+               </div>
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
+                    </svg>
+                </span>
+            </div>
+            
+            
         </div>
     );
 };
